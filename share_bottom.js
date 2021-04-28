f144860086359db06ab4cf50c8d0974e6e4e160b
kepohjmp <82988660+kepohjmp@users.noreply.github.com>
2021-04-28 08:44:24 +0300

    Create share_bottom.js

---
+++ share_bottom.js
@@ -0,0 +1,5 @@
+//<![CDATA[
+//www.gblog.id
+function loadCSS(e, t, n) { "use strict"; var i = window.document.createElement("link"); var o = t || window.document.getElementsByTagName("script")[0]; i.rel = "stylesheet"; i.href = e; i.media = "only x"; o.parentNode.insertBefore(i, o); setTimeout(function () { i.media = n || "all" }) }
+loadCSS("http://fonts.googleapis.com/css?family=Arimo:400italic,400,700");loadCSS("https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css");
+//]]>
